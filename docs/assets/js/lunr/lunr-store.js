33364dde927eb9163d4453cca167d51b00b92561
Andy <andrew.whitaker@fssa.in.gov>
2020-10-06 11:39:47 -0400

    Added Job Req Tracker Post

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -1,11 +1,16 @@
 var store = [{
+        "title": "Coming up for air",
+        "excerpt":"I’ve been wanting to start a blog for the last 5 years, but there has always been stuff to do, that at the time, seemed more important. I finally feel like I’m coming up for air after spending the last 5 years transitioning an organization from developing monolithic applications to...","categories": ["Team"],
+        "tags": ["Lessons Learned"],
+        "url": "https://visualizeit-consulting.github.io/astd/team/coming-up-for-air/",
+        "teaser":null},{
         "title": "Downtime's no fun - Time to build",
         "excerpt":"So here we are in October 2020. On September 30th I finished transitioning my latest project to another team and I am having some down time before starting a new job. I’m not used to down time, so I’m going to fill it by building something and trying out some...","categories": ["Microservices"],
         "tags": ["CQRS","Kubernetes","AxonIQ"],
         "url": "https://visualizeit-consulting.github.io/astd/microservices/downtimes-no-fun/",
         "teaser":null},{
-        "title": "Coming up for air",
-        "excerpt":"I’ve been wanting to start a blog for the last 5 years, but there has always been stuff to do, that at the time, seemed more important. I finally feel like I’m coming up for air after spending the last 5 years transitioning an organization from developing monolithic applications to...","categories": ["Team"],
-        "tags": ["Lessons Learned"],
-        "url": "https://visualizeit-consulting.github.io/astd/team/coming-up-for-air/",
+        "title": "The CQRS+ES Job Application Tracker",
+        "excerpt":"In my recent search for a new job, I’ve had the pleasure of interacting with some different job search sites. I’ve been frustrated at a couple of them, one in particular, that sent me a rejection a few minutes after I saved my application as a draft (I did not...","categories": ["Microservices"],
+        "tags": ["Requirements","Event Sourcing","Kubernetes","DDD"],
+        "url": "https://visualizeit-consulting.github.io/astd/microservices/job-req-application/",
         "teaser":null}]
