7f82f13d438b4ea230125416fe678631aab74445
Andy <andrew.whitaker@fssa.in.gov>
2020-10-15 10:34:23 -0400

    Changed to custom domain

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2,25 +2,25 @@ var store = [{
         "title": "Coming up for air",
         "excerpt":"I’ve been wanting to start a blog for the last 5 years, but there has always been stuff to do, that at the time, seemed more important. I finally feel like I’m coming up for air after spending the last 5 years transitioning an organization from developing monolithic applications to...","categories": ["Team"],
         "tags": ["Lessons Learned"],
-        "url": "https://visualizeit-consulting.github.io/astd/team/coming-up-for-air/",
+        "url": "https://visualizeit-consulting.com/astd/team/coming-up-for-air/",
         "teaser":null},{
         "title": "Downtime's no fun - Time to build",
         "excerpt":"So here we are in October 2020. On September 30th I finished transitioning my latest project to another team and I am having some down time before starting a new job. I’m not used to down time, so I’m going to fill it by building something and trying out some...","categories": ["Microservices"],
         "tags": ["CQRS","Kubernetes","AxonIQ"],
-        "url": "https://visualizeit-consulting.github.io/astd/microservices/downtimes-no-fun/",
+        "url": "https://visualizeit-consulting.com/astd/microservices/downtimes-no-fun/",
         "teaser":null},{
         "title": "The CQRS+ES Job Application Tracker",
         "excerpt":"In my recent search for a new job, I’ve had the pleasure of interacting with some different job search sites. I’ve been frustrated at a couple of them, one in particular, that sent me a rejection a few minutes after I saved my application as a draft (I did not...","categories": ["Microservices"],
         "tags": ["Requirements","Event Sourcing","Kubernetes","DDD"],
-        "url": "https://visualizeit-consulting.github.io/astd/microservices/job-req-application/",
+        "url": "https://visualizeit-consulting.com/astd/microservices/job-req-application/",
         "teaser":null},{
         "title": "A Detour to Personas and Journeys",
         "excerpt":"So I might have gotten a little ahead of myself. I meant this post to be about story mapping the job board application. When I sat down to do that, I realized I left off a couple of other steps that should/could to be done prior to creating a Story...","categories": ["Microservices"],
         "tags": ["Story Mapping","UX"],
-        "url": "https://visualizeit-consulting.github.io/astd/microservices/detour-to-personas-and-journeys/",
+        "url": "https://visualizeit-consulting.com/astd/microservices/detour-to-personas-and-journeys/",
         "teaser":null},{
         "title": "Story Mapping the Job Board",
         "excerpt":"Now that the detour is over, we can focus on creating a story map for the job board. Story mapping has been around for years, but I am still surprised by the number of people I work with who have never heard of it. So let’s recap. What is a...","categories": ["Microservices"],
         "tags": ["Story Mapping"],
-        "url": "https://visualizeit-consulting.github.io/astd/microservices/story-mapping-the-job-board/",
+        "url": "https://visualizeit-consulting.com/astd/microservices/story-mapping-the-job-board/",
         "teaser":null}]
